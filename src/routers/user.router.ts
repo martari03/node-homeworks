ce489b24b3cf2edc745c13adaca2985929e0b3a3
Marta <rishnyakmarta@gmail.com>
2023-03-27 16:14:59 +0300

    configs

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,35 +1,30 @@
 import { Router } from "express";
 
 import { userController } from "../controllers";
-import { userMiddleware } from "../middlewares";
+import { authMiddleware, userMiddleware } from "../middlewares";
 
 const router = Router();
 
 router.get("/", userController.getAll);
-router.post("/", userMiddleware.isValidCreate, userController.create);
 
 router.get(
   "/:userId",
+  authMiddleware.checkAccessToken,
   userMiddleware.isIdValid,
   userMiddleware.getByIdOrThrow,
   userController.getById
 );
 router.put(
   "/:userId",
+  authMiddleware.checkAccessToken,
   userMiddleware.isIdValid,
   userMiddleware.isValidUpdate,
   userMiddleware.getByIdOrThrow,
   userController.update
 );
-router.patch(
-  "/:userId",
-  userMiddleware.isIdValid,
-  userMiddleware.isValidPartialUpdate,
-  userMiddleware.getByIdOrThrow,
-  userController.update
-);
 router.delete(
   "/:userId",
+  authMiddleware.checkAccessToken,
   userMiddleware.isIdValid,
   userMiddleware.getByIdOrThrow,
   userController.delete
